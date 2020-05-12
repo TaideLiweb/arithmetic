65b54ece8c33bf5b515ee9bcb056bce90d11af4e
Lee, TaiDe | Ted | TWR <ar-taide.lee@rakuten.com>
2020-05-12 18:23:24 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+function InputBlock (props){
+  return(
+  <div>
+    <div className="flexbox">
+      <div>
+        <span>數字1</span>
+        <input placeholder="請輸入第一個數字" onChange = {props.changefirstText}></input>
+      </div>
+      <div>
+        <span>數字2</span>
+        <input placeholder="請輸入第二個數字" onChange = {props.changesecondText}></input>
+      </div>
+    </div>
+    <div className="flexbox">
+      <div>
+        <span>+</span>
+        <input type = "radio" value = "+" onChange = {props.changemethod} name = "method"></input>
+      </div>
+      <div>
+        <span>-</span>
+        <input type = "radio" value = "-" onChange = {props.changemethod} name = "method"></input>
+      </div>
+      <div>
+        <span>*</span>
+        <input type = "radio" value = "*" onChange = {props.changemethod} name = "method"></input>
+      </div>
+      <div>
+        <span>/</span>
+        <input type = "radio" value = "/" onChange = {props.changemethod} name = "method"></input>
+      </div>
+    </div>
+  </div>
+  )
+}
+function OutputBlock(){
+  return(
+    <p></p>
+    )
+}
 
 function App() {
+  let [firstText,setfirstText] = useState('')
+  let [secondText,setsecondText] = useState('')
+  let [method,setmethod] = useState('')
+
+  let changefirstText = (e)=>{setfirstText(e.target.value)}
+  let changesecondText = (e)=>{setsecondText(e.target.value)}
+  let changemethod = (e)=>{setmethod(e.target.value)}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>四則運算</h2>
+      <InputBlock changefirstText = {changefirstText} changesecondText = {changesecondText} changemethod = {changemethod}/>
+      <OutputBlock firstText = {firstText} secondText = {secondText} method = {method}/>
     </div>
   );
 }
