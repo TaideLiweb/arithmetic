239a203e04b434bab8585dcfebf495c52d75c8df
Lee, TaiDe | Ted | TWR <ar-taide.lee@rakuten.com>
2020-05-13 09:24:10 +0800

    Updates

---
+++ precache-manifest.f26d86ed8db9909d415dde2b0f63cb60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02148ab42f913ac00d99d68299dcd835",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f4396a01b20c5543a86",
+    "url": "./static/css/main.ba9e3bd0.chunk.css"
+  },
+  {
+    "revision": "8d176e99bbf853ab30b2",
+    "url": "./static/js/2.6f8ac7dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6f8ac7dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4396a01b20c5543a86",
+    "url": "./static/js/main.c782520f.chunk.js"
+  },
+  {
+    "revision": "2c0603feae5901914430",
+    "url": "./static/js/runtime-main.43663257.js"
+  }
+]);
