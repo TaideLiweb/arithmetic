20d29624825c8dbc67d019b5b0bc2fea6a74946c
Lee, TaiDe | Ted | TWR <ar-taide.lee@rakuten.com>
2020-05-27 16:11:30 +0800

    add useEffect

--- src/component/Result.js
+++ src/component/Result.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 function Result(props) {
   const { firstText, secondText, method } = props;
-  let result;
-  switch (method) {
-    case '+':
-      result = Math.round(Number(firstText) + Number(secondText));
-      break;
-    case '-':
-      result = Math.round(firstText - secondText);
-      break;
-    case '*':
-      result = Math.round(firstText * secondText);
-      break;
-    case '/':
-      if (firstText.match('^0') && secondText.match('^0')) {
-        result = '無解';
-      } else if (firstText !== '' && firstText.match('^[0-9]*$') && secondText.match('^0')) {
-        result = '∞(無限大)';
-      } else if (!firstText.match('^[0-9]*$') || !secondText.match('^[0-9]*$')) {
-        result = '';
-      } else if (firstText === '' || secondText === '') {
-        result = '';
-      } else {
-        result = firstText / secondText;
-      } break;
-    default:
-  }
+  const [operatorValue, setOperatorValue] = useState('');
+  let result = 0;
+  const methodController = () => {
+    switch (method) {
+      case '+':
+        result = Math.round(Number(firstText) + Number(secondText));
+        break;
+      case '-':
+        result = Math.round(firstText - secondText);
+        break;
+      case '*':
+        result = Math.round(firstText * secondText);
+        break;
+      case '/':
+        if (firstText.match('^0') && secondText.match('^0')) {
+          result = '無解';
+        } else if (firstText !== '' && firstText.match('^[0-9]*$') && secondText.match('^0')) {
+          result = '∞(無限大)';
+        } else if (!firstText.match('^[0-9]*$') || !secondText.match('^[0-9]*$')) {
+          result = '';
+        } else if (firstText === '' || secondText === '') {
+          result = '';
+        } else {
+          result = firstText / secondText;
+        }
+        break;
+      default:
+    }
+  };
+  useEffect(() => {
+    methodController();
+    setOperatorValue(result);
+  }, [method, result, methodController]);
   return (
     <p className="minheight">
       答案為:
-      {result}
+      {operatorValue}
     </p>
   );
 }
